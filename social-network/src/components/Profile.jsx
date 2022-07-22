17d7cf3059fa8a6974d2ba4a4f9541853b778984
Yulianna Shulshenko <yulianna.zhitkovich@mai.ru>
2022-07-22 15:20:07 +0300

    start

---
+++ social-network/src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div> 
+                <img className="sea" src="https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
